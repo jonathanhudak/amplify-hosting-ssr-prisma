1539de31e3bf4b3ee4902f6a12f74302bfb06577
siegerts <stephen.siegert@gmail.com>
2022-06-21 11:26:58 -0400

    add api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import prisma from "@/lib/prisma";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const posts = await prisma.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+    },
+  });
+
+  res.status(200).json({ posts });
 }
