c10e35715b6597ceda95e3988a6eb3208f74e179
Jonathan Hudak <jmhudak@amazon.com>
2022-10-17 17:51:39 -0700

    Deploy

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 
-console.log(JSON.stringify(process.env, null, 2));
+// console.log(JSON.stringify(process.env, null, 2));
 
 export default async function handler(req, res) {
   const posts = await prisma.post.findMany({
