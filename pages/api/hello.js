b6b841f77dfd967ee3190c0297f42d73868335cb
Jonathan Hudak <jmhudak@amazon.com>
2022-10-17 17:34:31 -0700

    Debugging env variables

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
 
+console.log(JSON.stringify(process.env, null, 2));
+
 export default async function handler(req, res) {
   const posts = await prisma.post.findMany({
     select: {
